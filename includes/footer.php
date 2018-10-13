f1fa229c816ccfb16d2db6f675ca149d633f7107
kbengtson58 <33072503+kbengtson58@users.noreply.github.com>
2018-10-13 18:05:43 -0400

    Second push

---
+++ includes/footer.php
@@ -0,0 +1,6 @@
+<p>Bill Memorial Library<br>
+240 Monument Street<br>
+Groton, CT 06340<br>
+(860)445-0392</p>
+
+<p class="copyright">&copy; <?php echo date("Y") ?> Bill Memorial Library</p>
---
+++ includes/nav.php
@@ -0,0 +1,7 @@
+<ul>
+	<li> <a href="index.php">Home</a></li>
+	<li> <a href="about.php">About</a></li>
+	<li> <a href="programs.php">Programs</a></li>
+	<li> <a href="resources.php">Resources</a></li>
+	<li> <a href="contact.php">Contact</a></li>
+</ul>
